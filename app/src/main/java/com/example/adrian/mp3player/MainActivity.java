00752e3ea75a92d53fed79309894d34247c7fd39
Nossumus <majewski_adr@o2.pl>
2018-04-18 14:12:26 +0200

    Version_0.2.2
    Now there is custom notification that shows the title of song. Layout is changed too. I discarded unnesesery files.

--- app/src/main/java/com/example/adrian/mp3player/MainActivity.java
+++ app/src/main/java/com/example/adrian/mp3player/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.adrian.mp3player;
 
 import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.RemoteAction;
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.media.AudioManager;
@@ -10,16 +18,26 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.provider.SyncStateContract;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.RemoteViews;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,19 +45,23 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.util.ArrayList;
 
+
 public class MainActivity extends AppCompatActivity {
 
     //declaration section
     MediaPlayer mediaPlayer = new MediaPlayer();
     Handler handler = new Handler();
     TextView locationView;
+    TextView title;
+    private Button notifButton;
     private ImageButton backwardButton;
     private ImageButton pausePlayButton;
     private ImageButton forwardButton;
     SeekBar seekBar;
     String current_Location;
+    String current_Title;
     int ArraySize;
-    ArrayList<String> arrayList;
+    ArrayList<String> titleList;
     ArrayList<String> locationList;
     ListView listView;
     ArrayAdapter<String> adapter;
@@ -49,8 +71,18 @@ public class MainActivity extends AppCompatActivity {
     int progressValue;
     String songLength;
     TextView textView;
+    TextView notificationTitle;
+    private RemoteViews remoteViews;
+    private NotificationManager notificationManager;
+    NotificationCompat.Builder builder;
+    private int notification_id;
+    private RemoteAction remoteAction;
+    boolean condition = true;
+    public PendingIntent pendingIntent;
+    public static final String PAUSE = "com.example.adrian.mp3player.pausePlayAction";
 
-    public boolean checkPermissionForReadExtertalStorage() {
+
+    public boolean checkPermissionForReadExternalStorage() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             int result = context.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
             return result == PackageManager.PERMISSION_GRANTED;
@@ -60,10 +92,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void CreateAdapter_and_getLocation_also_setupOnClickListener() {
 
-        arrayList = new ArrayList<>();
+        titleList = new ArrayList<>();
         locationList = new ArrayList<>();
         getMusic();
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+
+        adapter = new ArrayAdapter<String>(this, R.layout.list_layout, titleList);
         listView.setAdapter(adapter);
 
 
@@ -72,46 +105,25 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
 
 
-                    mediaPlayer.stop();
-                    mediaPlayer.reset();
-
-
+                mediaPlayer.stop();
+                mediaPlayer.reset();
                 // getting number of elements in array
-                ArraySize = arrayList.size() - 1;
-
+                ArraySize = titleList.size() - 1;
                 // setting "Pseudo-cursor"
                 pseudoCursor = position;
                 // getting location of desired file
                 current_Location = locationList.get(pseudoCursor);
-
-
-                //list2 = (adapterView.getItemAtPosition(position).toString());// it will go back to start of an array
-               // onPauseLocation = list4.substring(list4.indexOf("/"));
-
-
-                // setting data source
-                try {
-                    //mediaPlayer.setDataSource(current_Location);
-                    locationView.setText(current_Location);
-                    mediaPlayer.setDataSource(getApplicationContext(), Uri.parse(current_Location));
-                }catch (IllegalStateException e){
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                //preparing media player
-                try {
-                    mediaPlayer.prepare();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                //run play/pause action
+                // encode location
+                current_Location = Uri.encode(current_Location);
+                // set title
+                current_Title = titleList.get(pseudoCursor);
+                notificationPlayer();
+                // set title text view
+                title.setText(current_Title);
+                // there will be error if current_Location have # sign inside
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), Uri.parse(current_Location));
                 pausePlayAction();
-                // seekBar.setMax(mediaPlayer.getDuration());
-
-
             }
-
         });
 
         forwardButton.setOnClickListener(new View.OnClickListener() {
@@ -136,12 +148,7 @@ public class MainActivity extends AppCompatActivity {
                 backwardAction();
             }
         });
-
-
-
     }
-
-
 
     public void pausePlayAction() {
 
@@ -154,44 +161,51 @@ public class MainActivity extends AppCompatActivity {
             seekBar();
             playCycle();
 
-          //  mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-          //      @Override
-          //      public void onCompletion(MediaPlayer mediaPlayer) {
-          //          mediaPlayer.release();
-           //     }
-           // });
+
+            //  mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            //      @Override
+            //      public void onCompletion(MediaPlayer mediaPlayer) {
+            //          mediaPlayer.release();
+            //     }
+            // });
 
         }
     }
 
     public void forwardAction() {
-            mediaPlayer.stop();
-            mediaPlayer.reset();
 
-            pseudoCursor++;
+        mediaPlayer.stop();
+        mediaPlayer.reset();
 
-            if(pseudoCursor > ArraySize)
-            {
-                // get back to the first song
-                pseudoCursor = 0;
-            }
+        pseudoCursor++;
 
-            current_Location = locationList.get(pseudoCursor);
-            try {
-                mediaPlayer.setDataSource(this, Uri.parse(current_Location));
-                locationView.setText(current_Location);
-                // mediaPlayer.setDataSource(next_Location);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            //preparing media player
-            try {
-                mediaPlayer.prepare();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            //run play/pause action
-            pausePlayAction();
+        if (pseudoCursor > ArraySize) {
+            // get back to the first song
+            pseudoCursor = 0;
+        }
+        // get location
+        current_Location = locationList.get(pseudoCursor);
+        // encode location
+        current_Location = Uri.encode(current_Location);
+        // set title
+        current_Title = titleList.get(pseudoCursor);
+        notificationPlayer();
+
+        try {
+            mediaPlayer.setDataSource(this, Uri.parse(current_Location));
+            // set title text view
+            title.setText(current_Title);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // preparing media player
+        try {
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // run play/pause action
+        pausePlayAction();
 
     }
 
@@ -201,16 +215,22 @@ public class MainActivity extends AppCompatActivity {
 
         pseudoCursor--;
 
-        if(pseudoCursor < 0)
-        {
+        if (pseudoCursor < 0) {
             // get back to the first song
             pseudoCursor = 0;
         }
-
+        // get location
         current_Location = locationList.get(pseudoCursor);
+        // encode location
+        current_Location = Uri.encode(current_Location);
+        // set title
+        current_Title = titleList.get(pseudoCursor);
+        // set data source
+        notificationPlayer();
         try {
             mediaPlayer.setDataSource(this, Uri.parse(current_Location));
-            locationView.setText(current_Location);
+            // set title text view
+            title.setText(current_Title);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -224,27 +244,30 @@ public class MainActivity extends AppCompatActivity {
         pausePlayAction();
 
     }
+
     public void seekBar() {
 
         seekBar.setMax(mediaPlayer.getDuration());
-        textView .setText(seekBar.getProgress() + "/" + seekBar.getMax());
+        textView.setText(seekBar.getProgress() + "/" + seekBar.getMax());
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean input) {
-                if(input){
+
+                if (input) {
                     mediaPlayer.seekTo(progress);
                 }
-                progressValue = (progress/1000);
-                if((seekBar.getMax()/1000)%60 < 10){
-                    songLength = (seekBar.getMax()/1000)/60 + ":0" + (seekBar.getMax()/1000)%60;
-                }else {
+                progressValue = (progress / 1000);
+                if ((seekBar.getMax() / 1000) % 60 < 10) {
+                    songLength = (seekBar.getMax() / 1000) / 60 + ":0" + (seekBar.getMax() / 1000) % 60;
+                } else {
                     songLength = (seekBar.getMax() / 1000) / 60 + ":" + (seekBar.getMax() / 1000) % 60;
                 }
-                if(progressValue%60 < 10){
-                    textView.setText(progressValue/60 + ":0" + progressValue % 60 + "/" + songLength);
-                }else{
-                textView.setText(progressValue/60 + ":" + progressValue % 60 + "/" + songLength);
-            }}
+                if (progressValue % 60 < 10) {
+                    textView.setText(progressValue / 60 + ":0" + progressValue % 60 + "/" + songLength);
+                } else {
+                    textView.setText(progressValue / 60 + ":" + progressValue % 60 + "/" + songLength);
+                }
+            }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
@@ -253,31 +276,31 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                textView.setText(progressValue/60 + ":" + progressValue % 60 + "/" + songLength);
-                if(progressValue%60 < 10){
-                    textView.setText(progressValue/60 + ":0" + progressValue % 60 + "/" + songLength);
-                }else {
+                textView.setText(progressValue / 60 + ":" + progressValue % 60 + "/" + songLength);
+                if (progressValue % 60 < 10) {
+                    textView.setText(progressValue / 60 + ":0" + progressValue % 60 + "/" + songLength);
+                } else {
                     textView.setText(progressValue / 60 + ":" + progressValue % 60 + "/" + songLength);
                 }
             }
         });
     }
 
-    public void playCycle(){
-            seekBar.setProgress(mediaPlayer.getCurrentPosition());
+    public void playCycle() {
+        seekBar.setProgress(mediaPlayer.getCurrentPosition());
 
-           if(mediaPlayer.isPlaying()){
-             Runnable runnable = new Runnable() {
-               @Override
-               public void run() {
-                  playCycle();
+        if (mediaPlayer.isPlaying()) {
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    playCycle();
                 }
 
-           };handler.postDelayed(runnable, 1);
+            };
+            handler.postDelayed(runnable, 1);
 
-          }
-         }
-
+        }
+    }
 
 
     public void getMusic() {
@@ -298,16 +321,38 @@ public class MainActivity extends AppCompatActivity {
                 currentTitle = songCursor.getString(songTitle);
                 currentArtist = songCursor.getString(songArtist);
                 currentLocation = songCursor.getString(songLocation);
-                arrayList.add("Title: " + currentTitle + "\n" + "Artist:" + currentArtist + "\n" + "Location: " + currentLocation);
+                titleList.add(currentTitle);
                 locationList.add(currentLocation);
+
 
             } while (songCursor.moveToNext());
 
         }
     }
 
+    public void notificationPlayer() {
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
 
+        remoteViews = new RemoteViews(getPackageName(), R.layout.notification_layout);
+        remoteViews.setImageViewResource(R.id.notificationPausePlayButton, R.drawable.play);
+        //remoteViews.setImageViewResource(R.id.pauseButton,R.mipmap.ic_launcher);
+        remoteViews.setTextViewText(R.id.nTitle, current_Title);
+
+        builder = new NotificationCompat.Builder(getApplicationContext(), "channel_1");
+        //builder.setCustomBigContentView(remoteViews);
+        builder.setCustomContentView(remoteViews);
+        builder.setSmallIcon(R.drawable.ic_action_name);
+        //builder.addAction(R.drawable.pause,current_Title,pendingIntent);
+
+        remoteViews.setOnClickPendingIntent(R.id.nTitle,pendingIntent);
+
+
+
+        notificationManager.notify(0, builder.build());
+
+
+    }
 
 
     @Override
@@ -315,32 +360,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkPermissionForReadExtertalStorage();
+        checkPermissionForReadExternalStorage();
 
         // find them, find them all!
+        title = (TextView) findViewById(R.id.title_view);
+        title.setSelected(true);
         listView = (ListView) findViewById(R.id.listView);
-        locationView = (TextView) findViewById(R.id.locationView);
+        // locationView = (TextView) findViewById(R.id.locationView);
         pausePlayButton = (ImageButton) findViewById(R.id.pauseButton);
         forwardButton = (ImageButton) findViewById(R.id.forwardButton);
         backwardButton = (ImageButton) findViewById(R.id.backwardButton);
         seekBar = (SeekBar) findViewById(R.id.seekBar);
-        textView = (TextView) findViewById(R.id.textView);
+        textView = (TextView) findViewById(R.id.timer);
 
 
-        //to see pause/play button when app is starting
+        // to see pause/play button when app is starting
         pausePlayButton.setBackgroundResource(R.drawable.play);
 
 
         CreateAdapter_and_getLocation_also_setupOnClickListener();
 
 
-
-
-
-
     }
-
 }
+
 
 
 
---
+++ app/src/main/java/com/example/adrian/mp3player/NotificationActivity.java
@@ -0,0 +1,39 @@
+package com.example.adrian.mp3player;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+/**
+ * Created by Adrian on 17.04.2018.
+ */
+
+
+
+public class NotificationActivity extends AppCompatActivity {
+
+    ImageButton imageButton;
+
+
+    @Override
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.notification_layout);
+
+        imageButton = (ImageButton) findViewById(R.id.notificationPausePlayButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction("com.example.adrian.mp3player");
+                intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+                sendBroadcast(intent);
+                Toast.makeText(getApplicationContext(), "Message Sent" ,Toast.LENGTH_LONG);
+            }
+        });
+
+    }
+}
